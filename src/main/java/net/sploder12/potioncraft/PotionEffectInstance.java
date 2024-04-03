50cf2464033da957fcaf237a3e23448b993788ae
Ahydul <ahydul1@gmail.com>
2024-04-03 20:43:57 +0200

    Revert "update to 1.20.4"
    This reverts commit 6260f5a557a513745b2656578231daf9315fb388.

--- src/main/java/net/sploder12/potioncraft/PotionEffectInstance.java
+++ src/main/java/net/sploder12/potioncraft/PotionEffectInstance.java
@@ -4,8 +4,6 @@ import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.potion.Potion;
-import net.minecraft.registry.Registries;
-import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -128,7 +126,7 @@ public class PotionEffectInstance {
     }
 
     public NbtCompound writeNbt(NbtCompound nbt) {
-        nbt.putString("Id", Registries.STATUS_EFFECT.getId(this.type).toString());
+        nbt.putInt("Id", StatusEffect.getRawId(this.type));
         this.writeTypelessNbt(nbt);
         return nbt;
     }
@@ -143,8 +141,8 @@ public class PotionEffectInstance {
 
     @Nullable
     public static PotionEffectInstance fromNbt(NbtCompound nbt) {
-        String id = nbt.getString("Id");
-        StatusEffect type = (StatusEffect) Registries.STATUS_EFFECT.get(Identifier.tryParse(id));
+        int id = nbt.getInt("Id");
+        StatusEffect type = StatusEffect.byRawId(id);
         return type == null ? null : fromNbt(type, nbt);
     }
 
